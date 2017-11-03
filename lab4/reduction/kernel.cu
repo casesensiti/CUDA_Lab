10d5afe172ac8fa62564a8443743f74d4884412b
Casesensiti <casesensiti@gmail.com>
2017-11-03 16:02:57 -0700

    reduction done

--- lab4/reduction/kernel.cu
+++ lab4/reduction/kernel.cu
@@ -17,34 +17,15 @@ __global__ void reduction(float *out, float *in, unsigned size)
     ********************************************************************/
 
     // INSERT KERNEL CODE HERE
+    // assume BLOCK_SIZE is power of 2
+	__shared__ float res[2 * BLOCK_SIZE];
+	int start = 2 * blockIdx.x * blockDim.x;
+	res[threadIdx.x] = in[start + threadIdx.x];
+	res[BLOCK_SIZE + threadIdx.x] = in[BLOCK_SIZE + start + threadIdx.x];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	for (int stride = BLOCK_SIZE; stride > 0; stride /= 2) {
+		__syncthreads();
+		if (threadIdx.x < stride) res[threadIdx.x] += res[threadIdx.x + stride];
+	}
+	out[blockIdx.x] = res[0];
 }
