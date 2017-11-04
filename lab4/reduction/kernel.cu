2edd75fb3b1955c7da5825f1963d9506c180102b
Yue Wang <yuwang@bender.engr.ucr.edu>
2017-11-03 18:50:48 -0700

    reduction finish

--- lab4/reduction/kernel.cu
+++ lab4/reduction/kernel.cu
@@ -20,12 +20,16 @@ __global__ void reduction(float *out, float *in, unsigned size)
     // assume BLOCK_SIZE is power of 2
 	__shared__ float res[2 * BLOCK_SIZE];
 	int start = 2 * blockIdx.x * blockDim.x;
-	res[threadIdx.x] = in[start + threadIdx.x];
-	res[BLOCK_SIZE + threadIdx.x] = in[BLOCK_SIZE + start + threadIdx.x];
+        if (start + threadIdx.x < size) 	
+            res[threadIdx.x] = in[start + threadIdx.x];
+        else res[threadIdx.x] = 0;
+	if (BLOCK_SIZE + start + threadIdx.x < size)
+            res[BLOCK_SIZE + threadIdx.x] = in[BLOCK_SIZE + start + threadIdx.x];
+        else res[BLOCK_SIZE + threadIdx.x] = 0;
 
 	for (int stride = BLOCK_SIZE; stride > 0; stride /= 2) {
 		__syncthreads();
 		if (threadIdx.x < stride) res[threadIdx.x] += res[threadIdx.x + stride];
 	}
-	out[blockIdx.x] = res[0];
+	if(threadIdx.x == 0) out[blockIdx.x] = res[0];
 }
