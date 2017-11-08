495bbd535386c5b4cc73d14616ad9153db0e2270
Yue Wang <yuwang@bender.engr.ucr.edu>
2017-11-08 10:24:50 -0800

    lab4 prefix-scan finish

--- lab4/prefix-scan/kernel.cu
+++ lab4/prefix-scan/kernel.cu
@@ -12,13 +12,17 @@
 // need to
 
 // INSERT KERNEL(S) HERE
-__global__ void partialScan(float *out, float *in, float *out_d, unsigned in_size) {
-	__shared__ buf[BLOCK_SIZE * 2];
+__global__ void partialScan(float *out, float *in, float *out_b, unsigned in_size) {
+	__shared__ float buf[BLOCK_SIZE * 2];
 	int tx = threadIdx.x, offset = blockIdx.x * blockDim.x * 2;
-	// note: should buf set to 0 if out of range?
-	if (tx + offset < in_size) buf[tx] = in[tx + offset];
-	if (tx + offset + BLOCK_SIZE < in_size) buf[tx + BLOCK_SIZE] = in[tx + offset + BLOCK_SIZE];
-
+        if (blockIdx.x == 0 && threadIdx.x == 0) {
+            buf[0] = 0;
+            if (BLOCK_SIZE < in_size) buf[BLOCK_SIZE] = in[BLOCK_SIZE - 1];
+        }
+        else {
+            if (tx + offset - 1 < in_size) buf[tx] = in[tx + offset - 1];
+	    if (tx + offset + BLOCK_SIZE - 1 < in_size) buf[tx + BLOCK_SIZE] = in[tx + offset + BLOCK_SIZE - 1];
+        }
 	for (int stride = 1; stride <= BLOCK_SIZE; stride *= 2) {
 		__syncthreads();
 		int i = (tx + 1) * stride * 2 - 1;
@@ -30,11 +34,8 @@ __global__ void partialScan(float *out, float *in, float *out_d, unsigned in_siz
 		if (i + stride < BLOCK_SIZE * 2) buf[i + stride] += buf[i];
 	}
 	__syncthreads();
-	// copy to out_d
-	if (!tx) {
-		if (blockIdx.x == blockDim.x - 1) out_d[0] = 0;
-		else out_d[blockIdx.x + 1] = buf[BLOCK_SIZE * 2 - 1];
-	}
+	// copy to out_b
+	if (!tx) out_b[blockIdx.x] = buf[BLOCK_SIZE * 2 - 1];
 	// copy to out
 	if (tx + offset < in_size) out[tx + offset] = buf[tx];
 	if (tx + offset + BLOCK_SIZE < in_size) out[tx + offset + BLOCK_SIZE] = buf[tx + BLOCK_SIZE];
@@ -43,7 +44,7 @@ __global__ void partialScan(float *out, float *in, float *out_d, unsigned in_siz
 __global__ void addVec(float *out, float *toAdd, unsigned in_size) {
 	int tx = threadIdx.x, offset = blockIdx.x * blockDim.x * 2;
 	if (tx + offset < in_size) out[tx + offset] += toAdd[blockIdx.x];
-	if (tx + offset + BLOCK_SIZE < in_size) out[tx + offset + BLOCK_SIZE] = toAdd[blockIdx.x];
+	if (tx + offset + BLOCK_SIZE < in_size) out[tx + offset + BLOCK_SIZE] += toAdd[blockIdx.x];
 }
 
 
@@ -64,7 +65,7 @@ void preScan(float *out, float *in, unsigned in_size)
 	cuda_ret = cudaMalloc((void**)&out_b, gridLen * sizeof(float));
     if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
     // launch kernel
-    partialScan <<<grodDim, blockDim>>> (out, in, out_d, in_size);
+    partialScan <<<gridDim, blockDim>>> (out, in, out_b, in_size);
 
     // preScan and plus back out_d if needed
     if (gridLen > 1) {
@@ -72,7 +73,7 @@ void preScan(float *out, float *in, unsigned in_size)
     	cuda_ret = cudaMalloc((void**)&out_bscaned, gridLen * sizeof(float));
     	if (cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
     	preScan(out_bscaned, out_b, gridLen);
-    	addVec <<<grodDim, blockDim>>> (out, out_bscaned, in_size);
+    	addVec <<<gridDim, blockDim>>> (out, out_bscaned, in_size);
     	cudaFree(out_bscaned);
     }
     cudaFree(out_b);
